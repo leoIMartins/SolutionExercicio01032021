258e108218ce3810b8f6987d8bb41b129bd6794b
Leonardo <leonardo_ignacio2010@hotmail.com>
2021-05-10 17:32:55 -0300

    Projeto orçamento

--- Orcamento/Model/Modulo.cs
+++ Orcamento/Model/Modulo.cs
@@ -13,11 +13,11 @@ namespace Model
         public string Ambiente { get; set; }
         public string Squad { get; set; }
         
-        public const string INSERT = "INSERT INTO TB_SQUAD (ambiente, squad) VALUES ('{0}', '{1}')";
-        public const string GETALL = "SELECT id, ambiente, squad FROM TB_SQUAD";
-        public const string UPDATE = "UPDATE TB_SQUAD SET ambiente = '{0}', squad = '{1}' WHERE id = {2}";
-        public const string DELETE = "DELETE FROM TB_SQUAD WHERE id = {0}";
-        public const string GETBYID = "SELECT id, ambiente, squad FROM TB_SQUAD WHERE id = {0}";
+        public const string INSERT = "INSERT INTO TB_MODULO (ambiente, squad) VALUES ('{0}', '{1}')";
+        public const string GETALL = "SELECT id, ambiente, squad FROM TB_MODULO";
+        public const string UPDATE = "UPDATE TB_MODULO SET ambiente = '{0}', squad = '{1}' WHERE id = {2}";
+        public const string DELETE = "DELETE FROM TB_MODULO WHERE id = {0}";
+        public const string GETBYID = "SELECT id, ambiente, squad FROM TB_MODULO WHERE id = {0}";
 
         #endregion
     }
